002ff74335df03158600eec36e0ebeb25c6af829
Anirban <geekay0184@gmail.com>
2024-01-17 18:40:53 +0600

    7 Digital Clock

---
+++ __7.Digital Clock/script.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", calculateTime);
+function calculateTime() {
+  let date = new Date();
+  let day = date.getDay();
+  let hours = date.getHours();
+  let munite = date.getMinutes();
+  console.log(day);
+  if(hours > 12) {
+    hours -= 12;
+  }
+  let amPm = hours <= 12 ? "PM" : "AM";
+  let days = [ "Sun", "Mon", "Tue", "Wed", "Thu","Fry", "Sat",];
+  let dayName = days[day];
+  console.log(dayName);
+  document.querySelector("#day").innerHTML = `${dayName}`;
+  document.querySelector("#hour").innerHTML = `${hours}`;
+  document.querySelector("#munite").innerHTML = `${munite}`;
+  document.querySelector("#pmam").innerHTML = `${amPm}`;
+
+  
+}
+setTimeout(calculateTime,1);
